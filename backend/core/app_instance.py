735b13bb7780eccb902afecdf2e977b080e9af1c
donghwi kim <gimdong3425@naver.com>
2025-11-02 01:14:56 +0900

    test

--- backend/core/app_instance.py
+++ backend/core/app_instance.py
@@ -1,6 +1,15 @@
 # core/app_instance.py
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 import os
 
 app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
 app.secret_key = os.getenv("FLASK_SECRET_KEY", "default_secret_key")
+
+if os.getenv("FLASK_ENV") == "production":
+    app.config["SESSION_COOKIE_SECURE"] = True
+    app.config["SESSION_COOKIE_HTTPONLY"] = True
+    app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
